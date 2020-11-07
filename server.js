4d84834f2856d41391e1b26a2a70c82a371611af
JP <jpjones22@gmail.com>
2020-11-06 20:00:13 -0800

    NPM install

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 'use strict';
 
-//require('dotenv').config();
+const { response } = require('express');
+//Third party dependency.  Pulling it in to use it
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// allows express to serve "static files" (imgs, text...)
 app.use(express.static('./public'));
 
-app.get('/hello', (request, response) => {
-  response.status(200).send('Hello');
+//GET route
+
+app.get('/about-us', (request, response) => {
+  response.send('about-us');
 });
 
-app.get('/data', (request, response) => {
-  let airplanes = {
-    departure: Date.now(),
-    canFly: true,
-    pilot: 'Well Trained',
-  };
-  response.status(200).json(airplanes);
+//api route
+app.get('/routeTester', (request, response) => {
+  response.json('msg: this is what you want');
+});
+//Send back files
+app.get('/', (req, res) => {
+  response.sendFile('./public/index.html');
 });
 
-app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
+app.listen(PORT, () => {
+  console.log('Server up on Port 3000'); //Use over 1000 (most use 3000<= arbitrary standard)
+});
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+
